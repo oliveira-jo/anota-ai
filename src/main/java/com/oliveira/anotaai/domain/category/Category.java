9a93f0130809fe89ebb07ce5d440ad1187f42d7d
MacPro <macpro@MacBook-Pro-de-MacPro.local>
2024-04-12 15:25:04 -0300

    Refactor Product and Category

--- src/main/java/com/oliveira/anotaai/domain/category/Category.java
+++ src/main/java/com/oliveira/anotaai/domain/category/Category.java
@@ -1,15 +1,18 @@
 package com.oliveira.anotaai.domain.category;
 
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Document(collation = "categories")
+@Document(collection = "categories")
 @Getter
 @Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Category {
   @Id
@@ -22,5 +25,26 @@ public class Category {
     this.title = categoryDTO.title();
     this.description = categoryDTO.description();
     this.ownerId = categoryDTO.ownerId();
+  }
+
+  @Override
+  public String toString() {
+    JSONObject json = new JSONObject();
+    json.put("id", id);
+    json.put("title", title);
+    json.put("description", description);
+    json.put("ownerId", ownerId);
+    json.put("type", "categoria");
+
+    return json.toString();
+  }
+
+  public String deleteToString() {
+    JSONObject json = new JSONObject();
+    json.put("id", this.id);
+    json.put("ownerId", this.ownerId);
+    json.put("type", "delete-categoria");
+
+    return json.toString();
   }
 }
--- src/main/java/com/oliveira/anotaai/domain/product/Product.java
+++ src/main/java/com/oliveira/anotaai/domain/product/Product.java
@@ -1,17 +1,19 @@
 package com.oliveira.anotaai.domain.product;
 
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
-
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 import com.oliveira.anotaai.domain.category.Category;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Document(collection = "products")
 @Getter
 @Setter
 @NoArgsConstructor
-
 public class Product {
   @Id
   private String id;
@@ -19,6 +21,7 @@ public class Product {
   private String description;
   private String ownerId;
   private Integer price;
+  @DBRef
   private Category category;
 
   public Product(ProductDTO productDTO) {
@@ -26,6 +29,29 @@ public class Product {
     this.description = productDTO.description();
     this.ownerId = productDTO.ownerId();
     this.price = productDTO.price();
+  }
+
+  @Override
+  public String toString() {
+    JSONObject json = new JSONObject();
+    json.put("id", id);
+    json.put("title", title);
+    json.put("description", description);
+    json.put("ownerId", ownerId);
+    json.put("price", price);
+    json.put("category", category);
+    json.put("type", "produto");
+
+    return json.toString();
+  }
+
+  public String deleteToString() {
+    JSONObject json = new JSONObject();
+    json.put("id", this.id);
+    json.put("ownerId", this.ownerId);
+    json.put("type", "delete-produto");
+
+    return json.toString();
   }
 
 }
